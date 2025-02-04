15ea1c0199b068d3b2e36d6bc239f3de905bcfa1
Doru Blânzeanu <dblnz@pm.me>
2025-02-04 17:16:57 +0200

    add gdb debug tread creation method
    Signed-off-by: Doru Blânzeanu <dblnz@pm.me>

---
+++ src/hyperlight_host/src/hypervisor/gdb/mod.rs
@@ -0,0 +1,62 @@
+/*
+Copyright 2024 The Hyperlight Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+use std::io::{self, ErrorKind};
+use std::net::TcpListener;
+use std::thread;
+use thiserror::Error;
+#[derive(Debug, Error)]
+pub enum GdbTargetError {
+    #[error("Error encountered while binding to address and port")]
+    CannotBind,
+    #[error("Error encountered while listening for connections")]
+    ListenerError,
+    #[error("Unexpected error encountered")]
+    UnexpectedError,
+}
+
+impl From<io::Error> for GdbTargetError {
+    fn from(err: io::Error) -> Self {
+        match err.kind() {
+            ErrorKind::AddrInUse => Self::CannotBind,
+            ErrorKind::AddrNotAvailable => Self::CannotBind,
+            ErrorKind::ConnectionReset
+            | ErrorKind::ConnectionAborted
+            | ErrorKind::ConnectionRefused => Self::ListenerError,
+            _ => Self::UnexpectedError,
+        }
+    }
+}
+/// Creates a thread that handles gdb protocol
+pub fn create_gdb_thread(
+    port: u16,
+) -> Result<(), GdbTargetError> {
+    let socket = format!("localhost:{}", port);
+
+    log::info!("Listening on {:?}", socket);
+    let listener = TcpListener::bind(socket)?;
+
+    log::info!("Starting GDB thread");
+    let _handle = thread::Builder::new()
+        .name("GDB handler".to_string())
+        .spawn(move || -> Result<(), GdbTargetError> {
+            log::info!("Waiting for GDB connection ... ");
+            let (_, _) = listener.accept()?;
+            Ok(())
+        });
+
+    Ok(())
+}
