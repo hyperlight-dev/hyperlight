535dd755b992c441c118e9990ed4c93927a2c57d
Doru Blânzeanu <dblnz@pm.me>
2025-02-04 19:44:47 +0200

    add debug communication channel type
    - this type will be used by the gdb and the hypervisor handler
    to send requests and receive responses
    
    Signed-off-by: Doru Blânzeanu <dblnz@pm.me>

--- src/hyperlight_host/src/hypervisor/gdb/mod.rs
+++ src/hyperlight_host/src/hypervisor/gdb/mod.rs
@@ -1,6 +1,9 @@
+#![allow(dead_code)]
 use std::io::{self, ErrorKind};
 use std::net::TcpListener;
 use std::thread;
+
+use crossbeam_channel::{Receiver, Sender, TryRecvError};
 use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum GdbTargetError {
@@ -8,6 +11,10 @@ pub enum GdbTargetError {
     CannotBind,
     #[error("Error encountered while listening for connections")]
     ListenerError,
+    #[error("Error encountered when waiting to receive message")]
+    CannotReceiveMsg,
+    #[error("Error encountered when sending message")]
+    CannotSendMsg,
     #[error("Unexpected error encountered")]
     UnexpectedError,
 }
@@ -24,6 +31,48 @@ impl From<io::Error> for GdbTargetError {
         }
     }
 }
+/// Type that takes care of communication between Hypervisor and Gdb
+pub struct DebugCommChannel<T, U> {
+    /// Transmit channel
+    tx: Sender<T>,
+    /// Receive channel
+    rx: Receiver<U>,
+}
+
+impl<T, U> DebugCommChannel<T, U> {
+    pub fn unbounded() -> (DebugCommChannel<T, U>, DebugCommChannel<U, T>) {
+        let (hyp_tx, gdb_rx): (Sender<U>, Receiver<U>) = crossbeam_channel::unbounded();
+        let (gdb_tx, hyp_rx): (Sender<T>, Receiver<T>) = crossbeam_channel::unbounded();
+
+        let gdb_conn = DebugCommChannel {
+            tx: gdb_tx,
+            rx: gdb_rx,
+        };
+
+        let hyp_conn = DebugCommChannel {
+            tx: hyp_tx,
+            rx: hyp_rx,
+        };
+
+        (gdb_conn, hyp_conn)
+    }
+
+    /// Sends message over the transmit channel and expects a response
+    pub fn send(&self, msg: T) -> Result<(), GdbTargetError> {
+        self.tx.send(msg).map_err(|_| GdbTargetError::CannotSendMsg)
+    }
+
+    /// Waits for a message over the receive channel
+    pub fn recv(&self) -> Result<U, GdbTargetError> {
+        self.rx.recv().map_err(|_| GdbTargetError::CannotReceiveMsg)
+    }
+
+    /// Checks whether there's a message waiting on the receive channel
+    pub fn try_recv(&self) -> Result<U, TryRecvError> {
+        self.rx.try_recv()
+    }
+}
+
 /// Creates a thread that handles gdb protocol
 pub fn create_gdb_thread(
     port: u16,
