b1fe7ed17f611e30dc4254f3472fe035d8889d5a
danbugs <danilochiarlone@gmail.com>
2025-03-19 04:52:01 +0000

    [guest,tests/rust_guests] change guest library to conform to new APIs in common library and use CGM
    The host now defines a single undifferentiated memory region—the custom guest memory region (or, CGM)—that
    the guest can address as it sees fit. This commit changes the guest library to leverage that and perform
    said address using the APIs defined in the common library.
    
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- src/hyperlight_guest/src/chkstk.rs
+++ src/hyperlight_guest/src/chkstk.rs
@@ -17,7 +17,7 @@ limitations under the License.
 use core::arch::global_asm;
 use core::mem::size_of;
 
-use hyperlight_common::mem::RunMode;
+use hyperlight_common::hyperlight_peb::RunMode;
 
 use crate::guest_error::{set_invalid_runmode_error, set_stack_allocate_error};
 use crate::{MIN_STACK_ADDRESS, RUNNING_MODE};
