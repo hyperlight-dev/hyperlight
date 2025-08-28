5d745526b8babf1c6b9f84d7f4e37f96a6a04a8a
Doru Blânzeanu <dblnz@pm.me>
2025-08-28 15:32:23 +0300

    [trace-host] move trace related logic to separate module
    - This helps with keeping code separate and easily gating it out
    
    Signed-off-by: Doru Blânzeanu <dblnz@pm.me>

--- src/hyperlight_host/src/sandbox/uninitialized_evolve.rs
+++ src/hyperlight_host/src/sandbox/uninitialized_evolve.rs
@@ -34,10 +34,10 @@ use crate::mem::ptr_offset::Offset;
 use crate::mem::shared_mem::GuestSharedMemory;
 #[cfg(any(feature = "init-paging", target_os = "windows"))]
 use crate::mem::shared_mem::SharedMemory;
-#[cfg(feature = "trace_guest")]
-use crate::sandbox::TraceInfo;
 #[cfg(gdb)]
 use crate::sandbox::config::DebugInfo;
+#[cfg(feature = "trace_guest")]
+use crate::sandbox::trace::TraceInfo;
 #[cfg(target_os = "linux")]
 use crate::signal_handlers::setup_signal_handlers;
 use crate::{MultiUseSandbox, Result, UninitializedSandbox, log_then_return, new_error};
