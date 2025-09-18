7aa84bd3f29bb2270c0fcd2e6a82a904f8fe236d
James Sturtevant <jsturtevant@gmail.com>
2025-09-18 23:34:52 +0000

    Use box now that the enum contains xmm registers which are large
    Signed-off-by: James Sturtevant <jsturtevant@gmail.com>

--- src/hyperlight_host/src/hypervisor/hyperv_linux.rs
+++ src/hyperlight_host/src/hypervisor/hyperv_linux.rs
@@ -202,7 +202,7 @@ mod debug {
 
                                 e
                             })
-                            .map(|_| DebugResponse::ReadRegisters(regs))
+                            .map(|_| DebugResponse::ReadRegisters(Box::new(regs)))
                     }
                     DebugMsg::RemoveHwBreakpoint(addr) => Ok(DebugResponse::RemoveHwBreakpoint(
                         debug
