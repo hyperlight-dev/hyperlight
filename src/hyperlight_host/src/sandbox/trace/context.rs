7ff7b039656725969c344c27a06ee89dadcf0af5
Doru Blânzeanu <dblnz@pm.me>
2025-10-24 17:41:11 +0300

    [trace-guest] Add trace/events filtering based on max_log_level
    Signed-off-by: Doru Blânzeanu <dblnz@pm.me>

--- src/hyperlight_host/src/sandbox/trace/context.rs
+++ src/hyperlight_host/src/sandbox/trace/context.rs
@@ -131,7 +131,7 @@ impl TraceContext {
 
         let current_ctx = Span::current().context();
 
-        let span = tracing::trace_span!("call-to-guest");
+        let span = tracing::info_span!("call-to-guest");
         let _ = span.set_parent(current_ctx);
         let entered = span.entered();
 
@@ -324,7 +324,7 @@ impl TraceContext {
     }
 
     pub fn new_host_trace(&mut self, ctx: Context) {
-        let span = tracing::trace_span!("call-to-host");
+        let span = tracing::info_span!("call-to-host");
         let _ = span.set_parent(ctx);
         let entered = span.entered();
         self.host_spans.push(entered);
