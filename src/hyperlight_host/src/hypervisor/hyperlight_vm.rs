e03e79dfc4ea2e78446d680890751fd1a9b54a2f
Ludvig Liljenberg <4257730+ludfjig@users.noreply.github.com>
2025-11-24 10:05:54 -0800

    Introduce HyperlightVm as a transparent wrapper
    Signed-off-by: Ludvig Liljenberg <4257730+ludfjig@users.noreply.github.com>

---
+++ src/hyperlight_host/src/hypervisor/hyperlight_vm.rs
@@ -0,0 +1,99 @@
+/*
+Copyright 2025 The Hyperlight Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+use crate::Result;
+
+use crate::hypervisor::{Hypervisor, InterruptHandle};
+use crate::mem::memory_region::MemoryRegion;
+use crate::mem::mgr::SandboxMemoryManager;
+use crate::mem::ptr::RawPtr;
+use crate::mem::shared_mem::HostSharedMemory;
+use crate::sandbox::host_funcs::FunctionRegistry;
+
+use log::LevelFilter;
+use std::sync::{Arc, Mutex};
+
+#[derive(Debug)]
+pub(crate) struct HyperlightVm {
+    pub(crate) vm: Box<dyn Hypervisor>,
+}
+
+impl HyperlightVm {
+    pub(crate) fn new(inner: Box<dyn Hypervisor>) -> Self {
+        Self { vm: inner }
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    pub(crate) fn initialise(
+        &mut self,
+        peb_addr: RawPtr,
+        seed: u64,
+        page_size: u32,
+        mem_mgr: &mut SandboxMemoryManager<HostSharedMemory>,
+        host_funcs: &Arc<Mutex<FunctionRegistry>>,
+        guest_max_log_level: Option<LevelFilter>,
+        #[cfg(gdb)] dbg_mem_access_fn: Arc<Mutex<SandboxMemoryManager<HostSharedMemory>>>,
+    ) -> Result<()> {
+        self.vm.initialise(
+            peb_addr,
+            seed,
+            page_size,
+            mem_mgr,
+            host_funcs,
+            guest_max_log_level,
+            #[cfg(gdb)]
+            dbg_mem_access_fn,
+        )
+    }
+
+    pub(crate) unsafe fn map_region(&mut self, rgn: &MemoryRegion) -> Result<()> {
+        unsafe { self.vm.map_region(rgn) }
+    }
+
+    pub(crate) unsafe fn unmap_region(&mut self, rgn: &MemoryRegion) -> Result<()> {
+        unsafe { self.vm.unmap_region(rgn) }
+    }
+
+    pub(crate) fn get_mapped_regions(
+        &self,
+    ) -> Box<dyn ExactSizeIterator<Item = &MemoryRegion> + '_> {
+        self.vm.get_mapped_regions()
+    }
+
+    pub(crate) fn dispatch_call_from_host(
+        &mut self,
+        dispatch_func_addr: RawPtr,
+        mem_mgr: &mut SandboxMemoryManager<HostSharedMemory>,
+        host_funcs: &Arc<Mutex<FunctionRegistry>>,
+        #[cfg(gdb)] dbg_mem_access_fn: Arc<Mutex<SandboxMemoryManager<HostSharedMemory>>>,
+    ) -> Result<()> {
+        self.vm.dispatch_call_from_host(
+            dispatch_func_addr,
+            mem_mgr,
+            host_funcs,
+            #[cfg(gdb)]
+            dbg_mem_access_fn,
+        )
+    }
+
+    pub(crate) fn interrupt_handle(&self) -> Arc<dyn InterruptHandle> {
+        self.vm.interrupt_handle()
+    }
+
+    pub(crate) fn clear_cancel(&self) {
+        self.vm.clear_cancel()
+    }
+}
