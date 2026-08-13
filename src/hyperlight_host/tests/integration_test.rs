ff376f38b1a88ecd8c14fc37c2c977a4ee4d07ee
Tomasz Andrzejak <andreiltd@gmail.com>
2026-08-13 16:58:51 +0200

    fix: adjust test heap sizes
    This still will cause OOM as the test requires but will increase initial
    heap size so that virtqueus finish initialization.

--- src/hyperlight_host/tests/integration_test.rs
+++ src/hyperlight_host/tests/integration_test.rs
@@ -537,7 +537,7 @@ fn guest_malloc_abort() {
     });
 
     // allocate a vector (on heap) that is bigger than the heap
-    let heap_size = 0x8000;
+    let heap_size = 40 * 1024;
     let size_to_allocate = 0x10000;
     assert!(
         size_to_allocate > heap_size,
@@ -582,7 +582,7 @@ fn guest_outb_with_invalid_port_poisons_sandbox() {
 
 #[test]
 fn guest_panic_no_alloc() {
-    let heap_size = 0x8000;
+    let heap_size = 40 * 1024;
 
     let mut cfg = SandboxConfiguration::default();
     cfg.set_heap_size(heap_size);
